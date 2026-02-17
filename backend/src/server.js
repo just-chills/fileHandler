05e63efa0e71e3e8a8d52df3054b3ba85a7cc135
KumPong <ananyod.cha@spumail.net>
2026-02-17 22:20:12 +0700

    Add Backend Path is View Ex File and Delete File

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,37 +8,58 @@ const port = 5000;
 
 const uploadsDir = path.join(__dirname, 'uploads');
 
-app.use(cors()); // อนุญาตให Frontend เรียกใช š šงาน
-app.use(express.static(uploadsDir)); // ใหšเขšาถึงไฟลŤที่อัปโหลด
-// ตั้งคŠาที่เก็บไฟลŤอัปโหลด
+app.use(cors()); // อนุญาตให้ Frontend เรียกใช้งาน
+app.use(express.static(uploadsDir)); // ให้เข้าถึงไฟล์ที่อัปโหลด
+
+// ตั้งค้าที่เก็บไฟล์ที่อัปโหลด
 const storage = multer.diskStorage({
  destination: uploadsDir,
  filename: (req, file, cb) => {
  cb(null, file.originalname);
  }
 });
+
 app.get('/', (req, res) => {
  res.send('Backend eun smoothly');
 });
 
 const upload = multer({ storage });
-// อัปโหลดไฟลŤจาก Client -> Server
+
+// อัปโหลดไฟล์จาก Client -> Server
 app.post('/upload', upload.single('file'), (req, res) => {
  res.json({ message: 'File uploaded successfully', filename: req.file.filename });
 });
-// แสดงรายการไฟลŤที่มีในเซริŤฟเวอรŤ
+
+// แสดงรตัวอย่างไฟล์ที่อัปโหลด
+app.get('/file/:filename', (req, res) => {
+  const filePath = path.join(uploadsDir, req.params.filename);
+  res.sendFile(filePath);
+});
+
+// แสดงรายการไฟล์ที่มีในเซิร์ฟเวอร์
 app.get('/files', (req, res) => {
  fs.readdir(uploadsDir, (err, files) => {
  if (err) return res.status(500).json({ error: 'Unable to list files' });
  res.json(files);
  });
 });
-// ใหš Client ดาวนŤโหลดไฟลŤจาก Server
+
+// ให้ Client ดาวน์โหลดไฟล์จาก Server
 app.get('/download/:filename', (req, res) => {
  const filePath = path.join(uploadsDir, req.params.filename);
  res.download(filePath);
 });
-// เริ่มเซิรŤฟเวอรŤ
+
+// ลบไฟล์จาก Server
+app.delete('/delete/:filename', (req, res) => {
+    const filePath = path.join(uploadsDir, req.params.filename);
+    fs.unlink(filePath, (err) => {
+        if (err) return res.status(500).json({ error: 'Unable to delete file' });
+        res.json({ message: 'File deleted successfully' });
+    });
+})
+
+// เริ่มเซิร์ฟเวอร์
 app.listen(port, () => {
  console.log(`Server running at http://localhost:${port}`);
 });
